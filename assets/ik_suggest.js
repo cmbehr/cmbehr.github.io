b1a321bf09ebcc9d277022116baf1345d7822a22
Behrendt <cmbehr@m-d25j302tdhjv.local>
2018-10-09 13:48:37 -0400

    keyboard use added

--- assets/ik_suggest.js
+++ assets/ik_suggest.js
@@ -119,7 +119,25 @@ var pluginName = "ik_suggest",
 		
 		plugin = event.data.plugin;
 		$me = $(event.currentTarget);
-			
+		switch (event.keyCode) {
+			case ik_utils.keys.down: // select next suggestion from list   
+						selected = plugin.list.find('.selected');  
+						if(selected.length) {
+							msg = selected.removeClass('selected').next().addClass('selected').text();
+						} else {
+							msg = plugin.list.find('li:first').addClass('selected').text();
+						}
+						plugin.notify.text(msg); // add suggestion text to live region to be read by screen reader
+						break;
+					case ik_utils.keys.up: // select previous suggestion from list
+						selected = plugin.list.find('.selected');
+						if(selected.length) {
+							msg = selected.removeClass('selected').prev().addClass('selected').text();
+						}
+						plugin.notify.text(msg);  // add suggestion text to live region to be read by screen reader    
+						break;
+				   
+					default: // get suggestions based on user input			
 				plugin.list.empty();
 				
 				suggestions = plugin.getSuggestions(plugin.options.source, $me.val());
@@ -134,7 +152,8 @@ var pluginName = "ik_suggest",
 				} else {
 					plugin.list.hide();
 				}
-
+			break;
+		}
 	};
 	
 	/** 
