3c3484eb2cc995f426c6600ebe7934cd7bf5cf67
Behrendt <cmbehr@m-d25j302tdhjv.local>
2018-10-09 14:48:02 -0400

    add keyboard to tooltip

--- assets/ik_tooltip.js
+++ assets/ik_tooltip.js
@@ -89,6 +89,24 @@
 					}
 										
 				})
+				.on('blur', function(event) {              
+					if (!$tooltip.hasClass('mouseover') ) { // hide tooltip if mouse is not over the current element               
+						$tooltip
+						    .attr({
+						        'aria-hidden': 'true'
+							    })
+						    .removeClass('visible');       
+					}
+				})
+				.on('keyup', function(event) {         
+					 if(event.keyCode == ik_utils.keys.esc) { // hide when escape key is pressed
+					     $tooltip
+					         .attr({
+						             'aria-hidden': 'true'
+					         })
+						.removeClass('visible');
+					}              
+				});
 		}
 	};
 	
