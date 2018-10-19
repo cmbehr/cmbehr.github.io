52ce033c6d4200ef05673e1b0e3b2397ce5b3764
Behrendt <cmbehr@m-d25j302tdhjv.local>
2018-10-19 14:09:54 -0400

    carousel

--- assets/ik_carousel.js
+++ assets/ik_carousel.js
@@ -2,7 +2,8 @@
 	
 	var pluginName = 'ik_carousel',
 		defaults = { // default settings
-			'animationSpeed' : 3000
+        'instructions': 'Carousel widget. Use left and reight arrows to navigate between slides.',
+		'animationSpeed' : 3000
 		};
 	 
 	/**
@@ -21,7 +22,7 @@
 		
 		this.init();
 	
-	};
+	}
 	
 	/** Initializes plugin. */
 	Plugin.prototype.init = function () {
@@ -34,14 +35,20 @@
 		
 		$elem
 			.attr({
-				'id': id
+				'id': id,
+			'role': 'region', // assign region role
+			'tabindex': 0, // add into the tab order
+			'aria-labelledby': id + '_instructions' // associate with instructions
 			})
 			.addClass('ik_carousel')
 			.on('mouseenter', {'plugin': plugin}, plugin.stopTimer)
-			.on('mouseleave', {'plugin': plugin}, plugin.startTimer)
+		    .on('keydown', {'plugin': plugin}, plugin.onKeyDown)
+			.on('mouseleave', {'plugin': plugin}, plugin.startTimer);
 		
 		$controls = $('<div/>')
-
+			.attr({
+				'aria-hidden': 'true' // hide controls from screen readers
+			})
 			.addClass('ik_controls')
 			.appendTo($elem);
 				
@@ -67,19 +74,32 @@
 				$me = $(el);
 				$src = $me.find('img').remove().attr('src');
 				
-				$me.css({
+				$me.attr({
+					'aria-hidden': 'true' // hide images from screen readers
+					})
+					.css({
 						'background-image': 'url(' + $src + ')'
 					});	
 				
 				$('<li/>')
 					.on('click', {'plugin': plugin, 'slide': i}, plugin.gotoSlide)
 					.appendTo($navbar);
+		
 			});
 		
 		plugin.navbuttons = $navbar.children('li');
 		plugin.slides.first().addClass('active');
 		plugin.navbuttons.first().addClass('active');
 		plugin.startTimer({'data':{'plugin': plugin}});
+		
+		$('<div/>') // add instructions for screen reader users
+			 .attr({
+					'id': id + '_instructions',
+					'aria-hidden': 'true'
+			})
+			.text(this.options.instructions)
+			.addClass('ik_readersonly')
+			.appendTo($elem);
 		
 	};
 	
@@ -103,6 +123,10 @@
 		
 		plugin.timer = setInterval(plugin.gotoSlide, plugin.options.animationSpeed, {'data':{'plugin': plugin, 'slide': 'right'}});
 		
+		if (event.type === 'focusout') {
+		   plugin.element.removeAttr('aria-live');
+		}	
+		
 	};
 	
 	/** 
@@ -117,6 +141,10 @@
 		var plugin = event.data.plugin;
 		clearInterval(plugin.timer);
 		plugin.timer = null;
+		
+	    if (event.type === 'focusin') {
+			plugin.element.attr({'aria-live': 'polite'});
+		}
 		
 	};
 	
@@ -144,7 +172,7 @@
 				direction = 'left';
 				n = index == 0 ? plugin.slides.length - 1 : --index;
 			} else {
-				direction = 'right'
+				direction = 'right';
 				n = index == plugin.slides.length - 1 ? 0 : ++index;
 			}
 			
@@ -166,17 +194,25 @@
 			next = event.data.next;
 			dir = event.data.dir;
 			
-			active.off( ik_utils.getTransitionEventName() )
+			active
+				.attr({
+				 'aria-hidden': 'true'
+				})
+			.off( ik_utils.getTransitionEventName() )
 				.removeClass(direction + ' active');
 				
-			next.removeClass('next')
-				.addClass('active');
+			next
+			   .attr({
+				  'aria-hidden': 'false'
+				})
+			   .removeClass('next')
+			   .addClass('active');
 			
 		});
 		
 		plugin.navbuttons.removeClass('active').eq(n).addClass('active');
 		
-	}
+	};
 	
 	$.fn[pluginName] = function ( options ) {
 		
@@ -189,6 +225,33 @@
 			
 		});
 		
-	}
+	};
 	
+	/**
+	* Handles keydown event on the next/prev links.
+	*
+	* @param {Object} event - Keyboard event.
+	* @param {object} event.data - Event data.
+	* @param {object} event.data.plugin - Reference to plugin.
+	*/
+	Plugin.prototype.onKeyDown = function (event) {
+		   
+		var plugin = event.data.plugin;
+		   
+		switch (event.keyCode) {
+			   
+			case ik_utils.keys.left:
+				event.data = {'plugin': plugin, 'slide': 'left'};
+				plugin.gotoSlide(event);
+				break;
+			case ik_utils.keys.right:
+				event.data = {'plugin': plugin, 'slide': 'right'};
+				plugin.gotoSlide(event);
+				break;
+			case ik_utils.keys.esc:
+				plugin.element.blur();
+				break;
+			}
+		};
+		
 })( jQuery, window, document );
